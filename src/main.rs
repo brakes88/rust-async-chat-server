0ee16a45a594b2203803b7c70d0ec65edd77be28
Riccardo Braga <braric88@gmail.com>
2026-04-02 17:07:36 +0200

    added tokio_select for concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use futures::{SinkExt, StreamExt};
-use tokio::net::TcpListener;
+use std::result::Result::Ok;
+use tokio::{
+    net::{TcpListener, TcpStream},
+    sync::broadcast::{self, Sender},
+};
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
 const HELP_MSG: &str = include_str!("help.txt");
@@ -7,26 +11,43 @@ const HELP_MSG: &str = include_str!("help.txt");
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:9876").await?;
+    let (tx, _) = broadcast::channel::<String>(32);
     loop {
-        let (mut tcp, _) = server.accept().await?;
-        let (reader, writer) = tcp.split();
-        let mut stream = FramedRead::new(reader, LinesCodec::new());
-        let mut sink = FramedWrite::new(writer, LinesCodec::new());
+        let (tcp, _) = server.accept().await?;
+        tokio::spawn(handle_user(tcp, tx.clone()));
+    }
+}
 
-        sink.send(HELP_MSG).await?;
+async fn handle_user(mut tcp: TcpStream, tx: Sender<String>) -> anyhow::Result<()> {
+    let (reader, writer) = tcp.split();
+    let mut stream = FramedRead::new(reader, LinesCodec::new());
+    let mut sink = FramedWrite::new(writer, LinesCodec::new());
 
-        while let Some(Ok(mut msg)) = stream.next().await {
-            // handle new /help command
-            if msg.starts_with("/help") {
-                sink.send(HELP_MSG).await?;
-            // handle new /quit command
-            } else if msg.starts_with("/quit") {
-                break;
-            // handle regular message
-            } else {
-                msg.push_str(" ❤️");
-                sink.send(msg).await?;
+    let mut rx = tx.subscribe();
+
+    sink.send(HELP_MSG).await?;
+
+    loop {
+        tokio::select! {
+            user_msg=stream.next()=>{
+                let mut user_msg = match user_msg {
+                    Some(msg) => msg?,
+                    None => break,
+                };
+                if user_msg.starts_with("/help") {
+                    sink.send(HELP_MSG).await?;
+                } else if user_msg.starts_with("/quit") {
+                    break;
+                } else {
+                    user_msg.push_str(" ❤️");
+                    let _ = tx.send(user_msg);
+                }
+            },
+            peer_msg=rx.recv()=>{
+                sink.send(peer_msg?).await?;
             }
+
         }
     }
+    Ok(())
 }
