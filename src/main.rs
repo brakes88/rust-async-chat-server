711b5d92ecd0c9d19ea23ac56e614934fa2b8509
Riccardo Braga <braric88@gmail.com>
2026-04-02 16:21:59 +0200

    added commands at start

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use futures::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
+const HELP_MSG: &str = include_str!("help.txt");
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:9876").await?;
@@ -10,9 +12,21 @@ async fn main() -> anyhow::Result<()> {
         let (reader, writer) = tcp.split();
         let mut stream = FramedRead::new(reader, LinesCodec::new());
         let mut sink = FramedWrite::new(writer, LinesCodec::new());
+
+        sink.send(HELP_MSG).await?;
+
         while let Some(Ok(mut msg)) = stream.next().await {
-            msg.push_str(" ❤️");
-            sink.send(msg).await?;
+            // handle new /help command
+            if msg.starts_with("/help") {
+                sink.send(HELP_MSG).await?;
+            // handle new /quit command
+            } else if msg.starts_with("/quit") {
+                break;
+            // handle regular message
+            } else {
+                msg.push_str(" ❤️");
+                sink.send(msg).await?;
+            }
         }
     }
 }
